9b1a8bd89cdd4a52dac66b3f32aa21b8bad9703c
Rusty DeGarmo <rustydegarmo@yahoo.com>
2021-07-23 20:28:12 -0500

    update to equipment age report and trek counter

--- module_11/equipment_age_counter.py
+++ module_11/equipment_age_counter.py
@@ -11,15 +11,21 @@ db = mysql.connector.connect(
 #create a cursor
 cursor = db.cursor()
 
-#Count how much equipment older than five years old
+#Count how much equipment is older than five years old
 cursor.execute('SELECT COUNT(*) FROM supplies WHERE CAST(date_added AS DATE) < NOW() - interval 5 year;')
-
 info = cursor.fetchall()
 
-
+print()
 print()
 print("-- DISPLAYING SUPPLIES RECORDS --")
 print(f"Equipment older than 5 years: {info[0][0]}")
+
+
+#Count how much equipment is newer than five years old
+cursor.execute('SELECT COUNT(*) FROM supplies WHERE CAST(date_added AS DATE) >= NOW() - interval 5 year;')
+infotwo = cursor.fetchall()
+print(f"Equipment 5 years old or newer: {infotwo[0][0]}")
+print()
 print()
 
 #close the database
--- module_11/trek_counter.py
+++ module_11/trek_counter.py
@@ -11,29 +11,56 @@ db = mysql.connector.connect(
 #create a cursor
 cursor = db.cursor()
 
+#create a view to pull the relevant data
+#cursor.execute('CREATE OR REPLACE VIEW customer_treks AS \
+#    SELECT
+#        ')
 #Count how many treks were to Africa     
 cursor.execute('SELECT COUNT(destination_name) FROM trip WHERE destination_name = "Africa";')
 info = cursor.fetchall()
 
+cursor.execute('SELECT arrival_date, departure_date FROM trip WHERE destination_name = "Africa"')
+africaTrips = cursor.fetchall()
 print()
 print("-- DISPLAYING AFRICA TREK RECORDS --")
 print(f"Number of treks to Africa: {info[0][0]}")
+print()
+print("Trip Dates: ")
+for date in africaTrips:
+    print(f"Arrival Date: {date[0]} Departure Date: {date[1]}")
+print()
 print()
 
 #Count how many treks were to Asia   
 cursor.execute('SELECT COUNT(destination_name) FROM trip WHERE destination_name = "Asia";')
 info = cursor.fetchall()
 
+cursor.execute('SELECT arrival_date, departure_date FROM trip WHERE destination_name = "Asia"')
+asiaTrips = cursor.fetchall()
+
 print("-- DISPLAYING ASIA TREK RECORDS --")
 print(f"Number of treks to Asia: {info[0][0]}")
+print()
+print("Trip Dates: ")
+for date in asiaTrips:
+    print(f"Arrival Date: {date[0]} Departure Date: {date[1]}")
+print()
 print()
 
 #Count how many treks were to Southern Europe
 cursor.execute('SELECT COUNT(destination_name) FROM trip WHERE destination_name = "Southern Europe";')
 info = cursor.fetchall()
 
+cursor.execute('SELECT arrival_date, departure_date FROM trip WHERE destination_name = "Southern Europe"')
+seTrips = cursor.fetchall()
+
 print("-- DISPLAYING SOUTHERN EUROPE TREK RECORDS --")
 print(f"Number of treks to Southern Europe: {info[0][0]}")
+print()
+print("Trip Dates: ")
+for date in seTrips:
+    print(f"Arrival Date: {date[0]} Departure Date: {date[1]}")
+print()
 print()
 
 #close the database
